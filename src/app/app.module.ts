ac9ed6cfac55fc0976d0e9a9cdd480cbc2926833
Wassim Sfar <wassim.sfar11@gmail.com>
2019-11-30 23:30:42 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list'
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from'@angular/material/button';
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
 
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component' ;
+import {DishService} from './services/dish.service'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
+
   ],
-  providers: [],
+  providers: [
+    DishService //This is for inject  service
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
